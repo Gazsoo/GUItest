b8cf8724f15c103799bc3e58738759781af453a4
csirmazg <csirmazg@gmail.com>
2022-02-06 22:52:46 +0100

    commit test

--- buildParsing.py
+++ buildParsing.py
@@ -3,25 +3,67 @@
 temp_path = "C/:"
 
 # build_parsing
+import os
 import xml.etree.ElementTree as ET
 import time
 t = time.process_time()
+
+# os.system("ping google.com")
 
 class XMLDataObject():
     project_name = ""
     build_dir = ""
     work_dir = ""
 
+    def __init__(self,hello) -> None:
+        self.build_dir = hello["build.dir"]
 
 
 def parse_data ():
     tree = ET.parse('./test_files/build.xml')
     root = tree.getroot()
-    print("it : ", root.tag)
-    print(root.attrib)
 
-    for child in root:
-        print(child.tag, child.attrib)
+    my_data = {}
+    #print(tree)
+    hello = (root.findall("property"))
+    for items in hello:
+        try: 
+            first_value_name = items.attrib["name"]
+        except:
+            first_value = 0
+            print("An exception occurred at: name, ", items.attrib) 
+        try: 
+            first_value = items.attrib["location"]
+        except:
+            try: 
+                first_value = items.attrib["value"]
+            except:
+                first_value = 0
+                print("An exception occurred at: value, ", items.attrib) 
+
+        my_data[first_value_name] = first_value
+
+    Initinit = XMLDataObject(my_data)
+    
+    
+    print(Initinit.build_dir)
+    for valami in my_data:
+        print(valami," ------> ", my_data[valami])
+
+    #print (hello[0].text)
+    # for h in hello:
+    #     print (h.text)
+
+    #print (hello)
+    # print(root.attrib)
+    # for child in root:
+    #     print (root.Element)
+    # print (child)
+
+    # for x in root:
+    #     #if x.tag == "project":
+    #     print(x.tag, x.attrib)
+
 
 parse_data()
 
