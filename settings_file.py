8ddce0aef023366803f69c6903436e58472c3937
csirmazg <csirmazg@gmail.com>
2022-02-09 23:10:48 +0100

    add settings file

---
+++ settings_file.py
@@ -0,0 +1,47 @@
+'''Settings file in appdata'''
+
+import json
+import os
+from appdirs import user_data_dir 
+
+APPNAME = "PythonXMLBuilder"
+APPAUTHOR = "EPTAR"
+SETTINGSFILE = "setting.json"
+
+
+def start_json_settings():
+    # Sample deffault data to save
+    settings_dict = {'employees' : [{'name' : 'John Doe','department' : 'Marketing','place' : 'Remote'},{'name' : 'Jane Doe','department' : 'Software Engineering','place' : 'Remote'},{ 'name' : 'Don Joe','department' : 'Software Engineering','place' : 'Office'}]}
+
+    appdata_path = user_data_dir(APPNAME, APPAUTHOR)
+    settings_path = os.path.join(appdata_path, SETTINGSFILE)
+
+    settings_dict = initiate_json_settings(settings_path, settings_dict, appdata_path)
+
+    return settings_dict
+
+# Make settings file
+# If exists, read. if not, make one to the appdata and put default in to it.
+def initiate_json_settings(settings_path, settings_dict, appdata_path):
+
+    # Check for folder and make if not present
+    if os.path.exists(appdata_path) :
+        print('Directory already exists')
+    else:
+        try:
+            os.makedirs(appdata_path)
+        except OSError:
+            print ("Creation of the directory %s failed" % appdata_path)
+        else:
+            print ("Successfully created the directory %s " % appdata_path)
+
+    if os.path.exists(settings_path) :
+    # Read settings
+        with open(settings_path, 'r') as out_file:
+            settings_dict = json.load(out_file)
+    else:
+    # Make a save settings file
+        with open(settings_path, 'w') as out_file:
+            json.dump(settings_dict, out_file, indent=4,ensure_ascii=False)
+
+    return settings_dict
